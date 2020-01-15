ded5ae7efdce9b1c95a454325401eb7602cfdb4e
prongbang <prongbang@gmail.com>
2020-01-16 00:46:14 +0700

    show multi route and distance & duration

--- README.md
+++ README.md
@@ -13,6 +13,15 @@
 -- https://stackoverflow.com/questions/39230575/google-maps-api-fastest-route-around-traffic
 -- https://stackoverflow.com/questions/23605262/google-maps-directions-in-current-traffic-with-waypoints
 -- https://stackoverflow.com/questions/41672521/how-to-show-real-time-traffic-data-for-a-particular-route-on-maps
+-- https://developers.google.com/maps/documentation/javascript/directions#DirectionsRequests
+
+- Driving Options
+-- https://developers.google.com/maps/documentation/javascript/get-api-key#client-id
+
+
+### Direction API for Android
+
+- https://stackoverflow.com/questions/20979853/getting-distance-from-google-direction-api-in-android
 
 
 #### Reference
--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var directionsDisplay;
 new Vue({
   el: "#app",
   data: {
-    originLatLng: "",
-    destinationLatLng: "",
-    locations: []
+    originLatLng: "13.7469267, 100.5327943",
+    destinationLatLng: "13.7430621, 100.5466558",
+    directionRoutes: []
   },
   created() {
     setTimeout(() => {
@@ -18,6 +18,9 @@ new Vue({
     initMap() {
       directionsService = new google.maps.DirectionsService();
       directionsDisplay = new google.maps.DirectionsRenderer();
+      var directionsRenderer = new google.maps.DirectionsRenderer({
+        suppressMarkers: true
+      });
 
       var mapCenter = new google.maps.LatLng(13.736717, 100.523186);
 
@@ -27,29 +30,79 @@ new Vue({
       };
 
       map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      directionsRenderer.setMap(map);
 
-      var mapOrigin1 = new google.maps.LatLng(13.7469267,100.5327943);
-      var mapDestination1 = new google.maps.LatLng(13.7430621,100.5466558);
+      var mapOrigin1 = new google.maps.LatLng(13.7469267, 100.5327943);
+      var mapDestination1 = new google.maps.LatLng(13.7430621, 100.5466558);
 
       var mapOrigin2 = new google.maps.LatLng(46.5476592, 26.515106);
       var mapDestination2 = new google.maps.LatLng(46.4444641, 27.362008);
 
-      //   this.calculateRoute(mapOrigin1, mapDestination1);
-      //   this.calculateRoute(mapOrigin2, mapDestination2);
+      // this.calculateRoute(mapOrigin1, mapDestination1);
+      // this.calculateRoute(mapOrigin2, mapDestination2);
     },
     calculateRoute(mapOrigin, mapDestination) {
+      let self = this;
       var request = {
         origin: mapOrigin,
         destination: mapDestination,
-        travelMode: "DRIVING"
+        provideRouteAlternatives: true,
+        travelMode: google.maps.TravelMode.DRIVING,
+        drivingOptions: {
+          departureTime: new Date(/* now, or future date */),
+          trafficModel: "pessimistic" // optimistic
+        },
+        unitSystem: google.maps.UnitSystem.IMPERIAL
       };
-      directionsService.route(request, function(result, status) {
-        if (status == "OK") {
-          var directionsDisplay = new google.maps.DirectionsRenderer({
-            map: map
-          });
-          directionsDisplay.setDirections(result);
-        }
+
+      return new Promise((resolve, reject) => {
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            let routes = response.routes.sort((a, b) => {
+              return b.legs[0].distance.value - a.legs[0].distance.value;
+            });
+            response["routes"] = routes;
+
+            let directionRoutes = [];
+            for (var i = 0; i < routes.length; i++) {
+              let color = "#00b3fd";
+              let strokeWeight = 7;
+              if (i < routes.length - 1) {
+                color = self.getRandomColor();
+                strokeWeight = 6;
+              }
+
+              let directionRoute = {
+                color: color
+              };
+
+              var polyline = new google.maps.Polyline({
+                strokeColor: color,
+                strokeOpacity: 1,
+                strokeWeight: strokeWeight
+              });
+              var dr = new google.maps.DirectionsRenderer({
+                polylineOptions: polyline
+              });
+              dr.setDirections(response);
+              dr.setRouteIndex(i);
+              dr.setMap(map);
+
+              var directionsData = routes[i].legs[0]; // Get data about the mapped route
+              if (directionsData) {
+                let distance = directionsData.distance.text;
+                let duration = directionsData.duration.text;
+                directionRoute["distance"] = distance;
+                directionRoute["duration"] = duration;
+              }
+
+              directionRoutes.push(directionRoute);
+            }
+
+            resolve(directionRoutes);
+            // directionsDisplay.setDirections(response);
+          }
+        });
       });
     },
     addRoute() {
@@ -65,9 +118,13 @@ new Vue({
             destinations[0],
             destinations[1]
           );
-          this.calculateRoute(mapOrigin, mapDestination);
-
-          this.locations.push({origin: origins, des: destinations});
+          this.calculateRoute(mapOrigin, mapDestination)
+            .then(routes => {
+              this.directionRoutes = routes;
+            })
+            .catch(e => {
+              console.log(e);
+            });
 
           this.originLatLng = "";
           this.destinationLatLng = "";
@@ -77,6 +134,14 @@ new Vue({
       } else {
         alert("Please enter latitude,longitude");
       }
+    },
+    getRandomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
     }
   }
 });
